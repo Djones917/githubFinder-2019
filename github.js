3d27ad03e2c1547dc49528c1c1d0f759eaa8c893
Derek <nightwing917@hotmail.com>
2019-07-06 18:58:19 -0600

    adding profile response method

--- github.js
+++ github.js
@@ -3,4 +3,8 @@ class GitHub {
         this.client_id = 'a73356c13732c4f43a88';
         this.client_secret = '4123c810175a589805fa3f182973d29309a0fea3';
     }
+
+    async getUser(user) {
+       const profileResponse
+    }
 }
