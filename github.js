e6fb6c2c2ba92f30eb494ad7fa1b4a4f2447d8c8
Derek <nightwing917@hotmail.com>
2019-07-13 19:02:53 -0600

    github finder complete

--- github.js
+++ github.js
@@ -3,15 +3,21 @@ class Github {
     constructor() {
         this.client_id = 'a73356c13732c4f43a88';
         this.client_secret = '4123c810175a589805fa3f182973d29309a0fea3';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
